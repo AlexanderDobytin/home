62d364d1f02e974031abd9915a3ef955f9207e7a
alexanderDobytin <sibsubtech@gmail.com>
2017-03-11 16:19:24 +0300

    yandex

--- js/site.js
+++ js/site.js
@@ -224,3 +224,32 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+
+
+
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter43375879 = new Ya.Metrika({
+                    id:43375879,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+
+
